1e0aa0f24d138a2c31c71c93a7178b8c842951cb
Nikolai Chaourov <nik.chaourov@gmail.com>
2025-05-28 12:44:55 +1000

    Initial commit

---
+++ test/incidently-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IncidentlyCdk from '../lib/incidently-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/incidently-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IncidentlyCdk.IncidentlyCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
